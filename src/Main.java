06164062e4a113deb167d0902a613ca57bf574e4
Arnoud-Wilson <arnoud608@gmail.com>
2023-05-27 12:57:00 +0200

    added translator constructor and translate method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Hier mag je je code schrijven voor de hoofd-opdracht
+        Integer[] numeric = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        String[] alphabetic = {"één", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
 
+        Translator hoi = new Translator(numeric, alphabetic);
+
+        System.out.println(hoi);
+
+        System.out.println(hoi.translate(7));
 
     }
+
 }
---
+++ src/Translator.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class Translator {
+    HashMap<Integer, String> numericAlpha = new HashMap<>();
+
+
+    public Translator(Integer[] numeric, String[] alphabetic) {
+        for (int i = 0; i < numeric.length; i++) {
+            numericAlpha.put(numeric[i], alphabetic[i]);
+        }
+    }
+
+    public String translate(Integer number) {
+        return numericAlpha.get(number);
+    }
+
+    @Override
+    public String toString() {
+        return "Translator{" +
+                "numericAlpha=" + numericAlpha +
+                '}';
+    }
+}
